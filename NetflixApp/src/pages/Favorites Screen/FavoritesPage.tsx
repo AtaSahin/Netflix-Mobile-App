a78001d9fc526c5c59717ee82f318855473a2e29
Ata Sahin <atasahin05@gmail.com>
2023-06-30 23:00:17 +0300

    favorites page created

---
+++ NetflixApp/src/pages/Favorites Screen/FavoritesPage.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
+
+import type {RootState} from '../../App/store';
+import {useSelector, useDispatch} from 'react-redux';
+import {addFav} from '../../App/features/counter/counterSlice';
+
+function FavoritesPage(props) {
+  const [movies, setMovies] = useState([]);
+  const count = useSelector((state: RootState) => state.counter.favFilms);
+  const dispatch = useDispatch();
+  const favoriteMovies = movies.filter(movie => count.includes(movie.id));
+  console.log('Movies in favFilms state:', count);
+  return (
+    <View>
+      <Text onPress={() => console.log('Movies in favFilms state:', count)}>
+        {count}
+      </Text>
+    </View>
+  );
+}
+
+export default FavoritesPage;
